881a6314280c61e9aa080c85e2d999910f76f5ee
Slavicek <slavik1111@seznam.cz>
2018-09-24 11:51:22 +0200

    Fibonacciho posloupnost

--- src/PgrfFrame.java
+++ src/PgrfFrame.java
@@ -1,6 +1,14 @@
 public class PgrfFrame {
     public static void main(String... args) {
-        System.out.println("Hello world");
+        int k = 0, a = 0, b = 1;
+        System.out.print("0 1 ");
+        for (int i = 0; i < 15 ; i++) {
+            //pres sysout vypsat fibonacciho posloupnost
+            k = a +b;
+            System.out.print(k + " ");
+            a= b;
+            b = k;
+        }
     }
 
 }
